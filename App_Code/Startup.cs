194d1f9ca859e63f86b1121cbb57afe9f58c34c6
Shilpa Joy <shilpakjoy707@gmail.com>
2018-12-18 15:17:04 +0530

    Initial commit

---
+++ App_Code/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CafeManagement.Startup))]
+namespace CafeManagement
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
